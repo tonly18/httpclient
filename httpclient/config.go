f2884d214f2cbaee76f28423630b7fac44c098e5
tonly <tonly@163.com>
2023-09-26 16:55:08 +0800

    fix: http client

--- httpclient/config.go
+++ httpclient/config.go
@@ -21,15 +21,15 @@ var transport = &http.Transport{
 	TLSClientConfig: &tls.Config{
 		InsecureSkipVerify: true, //不校验服务端证书
 	},
-	DisableKeepAlives:   false, //禁用keep-alive
-	MaxConnsPerHost:     2000,  //每个Host(IP:PORT元组)能创建的最大连接数(默认值:0)
-	MaxIdleConnsPerHost: 1000,  //每个Host最大空闲数(能利用的链接数)(默认值:2)
-	DisableCompression:  true,  //禁止压缩
+	DisableKeepAlives:     false,            //禁用keep-alive
+	MaxConnsPerHost:       2000,             //每个Host(IP:PORT元组)能创建的最大连接数(默认值:0)
+	MaxIdleConns:          100,              //最大空闲数量(默认值:100)
+	MaxIdleConnsPerHost:   1000,             //每个Host最大空闲数(能复用的链接数)(默认值:2)
+	IdleConnTimeout:       90 * time.Second, //连接空闲超时(默认值:90)
+	DisableCompression:    true,             //禁止压缩
+	ExpectContinueTimeout: 1 * time.Second,  //(默认值:1)限制client在发送包含Expect:100-continue的header到收到继续发送body的response之间的时间等待
 
-	//ResponseHeaderTimeout: 1 * time.Second, //从client.request执行do之后,到server返回截至
+	//ResponseHeaderTimeout: 10 * time.Second, //从client.request执行do之后,到server返回截至
 	//ForceAttemptHTTP2: true,             //强制使用http2(默认值:true)
-	//MaxIdleConns:      100,              //最大空闲数量(默认值:100)
-	//IdleConnTimeout:   90 * time.Second, //连接空闲超时(默认值:90)
 	//TLSHandshakeTimeout:   10 * time.Second,  //TLS握手超时(默认值:10)
-	//ExpectContinueTimeout: 1 * time.Second, //(默认值:1)限制client在发送包含Expect:100-continue的header到收到继续发送body的response之间的时间等待
 }
