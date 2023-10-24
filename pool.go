3ba68aedad0a56ea87291c49a9018f4aca1675fb
tonly <tonly@163.com>
2023-10-24 14:04:13 +0800

    fix: pool

--- pool.go
+++ pool.go
@@ -7,20 +7,18 @@ import (
 
 const maxSize = 1 << 16 //64KiB
 
-var poolBuffer = sync.Pool{
+var poolBuffer = &sync.Pool{
 	New: func() any {
 		return bytes.NewBuffer(make([]byte, 0, 512))
 	},
 }
 
 func poolGet() *bytes.Buffer {
-	buffer := poolBuffer.Get().(*bytes.Buffer)
-	buffer.Reset()
-
-	return buffer
+	return poolBuffer.Get().(*bytes.Buffer)
 }
 
 func poolPut(buffer *bytes.Buffer) {
+	buffer.Reset()
 	if buffer.Cap() > maxSize {
 		return
 	}
